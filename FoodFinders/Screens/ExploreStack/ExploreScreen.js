274842c5c61434729681d230ad9a7d2f02ce9d45
DipenDB <madaradipen@gmail.com>
2021-05-10 15:06:23 +0545

    Dark Mode and Map is implemented

--- FoodFinders/Screens/ExploreStack/ExploreScreen.js
+++ FoodFinders/Screens/ExploreStack/ExploreScreen.js
@@ -1,27 +1,367 @@
 
 
-import React from 'react';
-import {View,Text,StyleSheet,StatusBar} from 'react-native';
+import React,{useContext} from 'react';
+import {View,Text,StyleSheet,Image,Dimensions,Platform,StatusBar} from 'react-native';
+import MapView, { PROVIDER_GOOGLE,Marker,Callout } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import {AuthContext} from "../../Store/Context/AuthContext";
+
+const { width, height } = Dimensions.get("window");
+const CARD_HEIGHT = 220;
+const CARD_WIDTH = width * 0.8;
+const SPACING_FOR_CARD_INSET = width * 0.1 - 10;
+
+
 
 const ExploreScreen=()=>{
+
+    const authContext = useContext(AuthContext)
+
     return(
         <View style={styles.container}>
-            <StatusBar style="auto" />
+            <MapView
+                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                style={styles.map}
+                customMapStyle={ authContext.isThemeDark ? mapDarkStyle : mapStandardStyle}
+                region={{
+                    latitude: 27.736687,
+                    longitude: 85.324633,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                }}
+            >
 
-            <Text>Explore Screen</Text>
+                <Marker
+                    draggable
+                    coordinate={{
+                        latitude: 27.736687,
+                        longitude: 85.324633,
+                    }}
+                    // image={require('../../Assets/Images/location.png')}
+                    title='First'
+                    description='This is first description.'
+                />
 
+
+
+
+            </MapView>
         </View>
-
     )
 }
+
+const mapDarkStyle=[
+    {
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#212121"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#757575"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#212121"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#757575"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.country",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#9e9e9e"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#bdbdbd"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#757575"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#181818"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#616161"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#1b1b1b"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#2c2c2c"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#8a8a8a"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#373737"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#3c3c3c"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#4e4e4e"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#616161"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#757575"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#3d3d3d"
+            }
+        ]
+    }
+]
+
+const mapStandardStyle=[]
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
+    map: {
+        flex: 1,
+    },
+    bubble:{
+        flexDirection:'row',
+        // alignSelf:'flex-start',
+        backgroundColor:'#fff',
+        borderRadius:6,
+        borderColor:'#ccc',
+        borderWidth:0.5,
+        padding:15,
+        width:150,
+    },
+    searchBox: {
+        position:'absolute',
+        marginTop: Platform.OS === 'ios' ? 40 : 20,
+        flexDirection:"row",
+        backgroundColor: '#fff',
+        width: '90%',
+        alignSelf:'center',
+        borderRadius: 5,
+        padding: 10,
+        shadowColor: '#ccc',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        elevation: 10,
+    },
+    chipsScrollView: {
+        position:'absolute',
+        top:Platform.OS === 'ios' ? 90 : 80,
+        paddingHorizontal:10
+    },
+    chipsIcon: {
+        marginRight: 5,
+    },
+    chipsItem: {
+        flexDirection:"row",
+        backgroundColor:'#fff',
+        borderRadius:20,
+        padding:8,
+        paddingHorizontal:20,
+        marginHorizontal:10,
+        height:35,
+        shadowColor: '#ccc',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        elevation: 10,
+    },
+    scrollView: {
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        paddingVertical: 10,
+    },
+    endPadding: {
+        paddingRight: width - CARD_WIDTH,
+    },
+    card: {
+        // padding: 10,
+        elevation: 2,
+        backgroundColor: "#FFF",
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+        marginHorizontal: 10,
+        shadowColor: "#000",
+        shadowRadius: 5,
+        shadowOpacity: 0.3,
+        shadowOffset: { x: 2, y: -2 },
+        height: CARD_HEIGHT,
+        width: CARD_WIDTH,
+        overflow: "hidden",
+    },
+    cardImage: {
+        flex: 3,
+        width: "100%",
+        height: "100%",
+        alignSelf: "center",
+    },
+    textContent: {
+        flex: 2,
+        padding: 10,
+    },
+    cardtitle: {
+        fontSize: 12,
+        // marginTop: 5,
+        fontWeight: "bold",
+    },
+    cardDescription: {
+        fontSize: 12,
+        color: "#444",
+    },
+    markerWrap: {
+        alignItems: "center",
+        justifyContent: "center",
+        width:50,
+        height:50,
+    },
+    marker: {
+        width: 30,
+        height: 30,
+    },
+    button: {
+        alignItems: 'center',
+        marginTop: 5
+    },
+    signIn: {
+        width: '100%',
+        padding:5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 3
+    },
+    textSign: {
+        fontSize: 14,
+        fontWeight: 'bold'
+    }
 });
 
 
